8a3fe41e663883f5a5cefd31fad111960ad8a3cd
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-02-05 23:11:54 +0100

    Add email scraping function definitions in views.py

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def scrape_emails_from_web(request):
+    pass
+
+def fetch_page_content(url):
+    pass
+
+def extract_emails_from_content(content):
+    pass
+
+def save_scraped_emails(emails):
+    pass
+
+def display_scraped_emails(request):
+    pass
